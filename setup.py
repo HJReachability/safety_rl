8d7540ef9c4bcb6b095d520d31120668fc609693
jfisac <jfisac@berkeley.edu>
2020-07-03 16:36:56 -0700

    Minor update to setup.py (format + dependencies).

--- setup.py
+++ setup.py
@@ -8,12 +8,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='safety_rl',
-    version='0.1',
-    description='This package uses the Safety Bellman Equation with modern reinforcement learning '
-                'techniques to find approximations of safe sets and safe policies',
+    version='0.2',
+    description=['This package uses the Safety Bellman Equation with modern',
+                 'reinforcement learning techniques to find approximations of',
+                 'safe sets and safe policies.'],
     author='Neil Lugovoy',
     author_email='nflugovoy@berkeley.edu',
     packages=find_packages(),
-    install_requires=['numpy', 'matplotlib', 'spinup', 'tensorflow==1.9.0', 'gym', 'scipy',
-                      'ray==0.7.3', 'pandas', 'opencv-python', 'psutil', 'lz4', 'Box2D', 'mujoco-py']
+    install_requires=['numpy', 'matplotlib', 'spinup', 'tensorflow==1.9.0',
+                      'gym', 'scipy', 'requests', 'ray==0.7.3', 'pandas',
+                      'opencv-python', 'psutil', 'lz4', 'Box2D', 'mujoco-py']
 )

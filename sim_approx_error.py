33aafd668972dbc2950b106e6c51eed868db1d8d
kaichiehhsu <kaichieh@princeton.edu>
2021-02-12 00:07:52 -0500

    polish code

--- sim_approx_error.py
+++ sim_approx_error.py
@@ -4,7 +4,7 @@
 #   about 5 open-loop attacker trajectories
 # 2. validate them by simulating a very large number of pursuer (defender) 
 #   trajectories, so that we can be confident that our system truly succeeds not 
-#   only against the “oracle adversary” predicted by the Q network, but also 
+#   only against the “oracle adversary” predicted by the Q-network, but also 
 #   against any possible adversary.
 
 
@@ -20,8 +20,26 @@ from utils.carPEAnalysis import *
 # Example: python3 sim_approx_error.py -nt 10
 
 def multi_experiment(env, agent, state, maxLength=50, numPursuerStep=10):
+    """
+    multi_experiment: simulate defender's trajectories and record the maximum
+        value among these trajectories.
+
+    Args:
+        env (object): environment.
+        agent (object): agent (DDQNPursuitEvasion here).
+        state (numpy array): initial state.
+        maxLength (int, optional): maximal length of the trajectory.
+            Defaults to 50.
+        numPursuerStep (int, optional): maximal length of action sequence taken
+            by the pursuer. Defaults to 10.
+
+    Returns:
+        [dict]: records maximal values, the corresponding action indices
+            sequence and trajectories of the evader and the pursuer.
+    """    
     print("I'm process", os.getpid())
-    responseDict = validateEvaderPolicy(env, agent, state, maxLength, numPursuerStep)
+    responseDict = validateEvaderPolicy(
+        env, agent, state, maxLength, numPursuerStep)
     maxminV = responseDict['maxminV']
     maxminIdx = responseDict['maxminIdx']
     info = responseDict[maxminIdx]
@@ -34,7 +52,7 @@ def multi_experiment(env, agent, state, maxLength=50, numPursuerStep=10):
     return dictTmp
 
 
-def run(args):
+def run(args):   
     #== ENVIRONMENT ==
     env = loadEnv(args)
     stateNum = env.state.shape[0]
@@ -44,15 +62,16 @@ def run(args):
 
     #== AGENT ==
     configFile = '{:s}/CONFIG.pkl'.format(args.modelFolder)
-    agent = loadAgent(args, configFile, device, stateNum, actionNum, numActionList)
+    agent = loadAgent(
+        args, configFile, device, stateNum, actionNum, numActionList)
 
     #== Getting states to be tested ==
     print('\n== Getting states to be tested ==')
     print('Load from {:s} ...'.format(args.resultFile))
     read_dictionary = np.load(args.resultFile, allow_pickle='TRUE').item()
     print(read_dictionary.keys())
-    ddqnValue     = read_dictionary['ddqnValue']
-    numSample     = read_dictionary['numSample']
+    ddqnValue = read_dictionary['ddqnValue']
+    numSample = read_dictionary['numSample']
 
     DDQNSucMtx = (ddqnValue <= 0)
     DDQNSucIndices = np.argwhere(DDQNSucMtx)
@@ -71,7 +90,7 @@ def run(args):
         state = samples[idx, np.arange(6)]
         states[cnt, :] = state
 
-    #! Customize
+    #== Estimating Approximation Error in Parallel ==
     from multiprocessing import Pool
     dictList = []
     numThread = args.numWorker
@@ -92,8 +111,8 @@ def run(args):
             maxLengthList      = [maxLength]      * numExp
             numPursuerStepList = [numPursuerStep] * numExp
 
-            subDictList = pool.starmap(multi_experiment, zip(
-                envList, agentList, stateList, maxLengthList, numPursuerStepList))
+            subDictList = pool.starmap(multi_experiment, zip( envList,
+                agentList, stateList, maxLengthList, numPursuerStepList))
         dictList = dictList + subDictList
 
     finalDict = {}
@@ -110,24 +129,22 @@ if __name__ == '__main__':
     np.set_printoptions(precision=2, suppress=True)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f",  "--forceCPU",    help="force CPU",
+    parser.add_argument("-f", "--forceCPU", help="force CPU",
         action="store_true")
-    parser.add_argument("-nt", "--numTest",   help="#tests",
+    parser.add_argument("-nt", "--numTest", help="#tests",
         default=10, type=int)
-    parser.add_argument("-nw", "--numWorker",   help="#workers",
-        default=6, type=int)
-    parser.add_argument("-ml", "--maxLength",   help="max length",
+    parser.add_argument("-nw", "--numWorker", help="#workers",
+        default=6,  type=int)
+    parser.add_argument("-ml", "--maxLength", help="max length",
         default=40, type=int)
-    parser.add_argument("-nps", "--numPursuerStep",   help="# pursuer steps",
+    parser.add_argument("-nps", "--numPursuerStep", help="#pursuer steps",
         default=10, type=int)
-    parser.add_argument("-of", "--outFile",     help="output file",
+    parser.add_argument("-of", "--outFile", help="output file",
         default='validationDict', type=str)
     parser.add_argument("-mf", "--modelFolder", help="model folder", 
         default='scratch/carPE/largeBuffer-2021-02-04-23_02', type=str)
     parser.add_argument("-rf", "--resultFile", help="result file", 
         default='data/largeBuffer.npy', type=str)
-
-
     args = parser.parse_args()
     print("\n== Arguments ==")
     print(args)

2b71fe2aa3f80cfdc4a27f08265e20a7d80a9bbe
kaichiehhsu <kaichieh@princeton.edu>
2021-04-25 12:31:20 -0400

    print

--- gym_reachability/gym_reachability/envs/dubins_car_one_cont.py
+++ gym_reachability/gym_reachability/envs/dubins_car_one_cont.py
@@ -409,7 +409,9 @@ class DubinsCarOneContEnv(gym.Env):
             policy.eval()
             state_tensor = torch.FloatTensor(state).to(self.device)
             u = policy(state_tensor).detach().cpu().numpy()
+            print(state, u)
             state = self.car.integrate_forward(state, u)
+            print(state)
         traj = np.array(traj)
         info = {'valueList':valueList, 'gxList':gxList, 'lxList':lxList}
         return traj, result, minV, info
